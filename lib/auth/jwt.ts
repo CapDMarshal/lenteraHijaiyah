7fe283792f037f77e20d7d23afd085c2aebc3065
Grezzt <andhika.prasetyo105@gmail.com>
2026-04-21 08:38:30 +0700

    feat: middleware auth

--- lib/auth/jwt.ts
+++ lib/auth/jwt.ts
@@ -3,7 +3,7 @@ import { SignJWT, jwtVerify } from "jose";
 const getJwtSecretKey = () => {
   // Kita gunakan AUTH_SECRET yang sudah Anda buat di file .env
   const secret = process.env.AUTH_SECRET;
-  
+
   if (!secret || secret.length === 0) {
     throw new Error("Missing AUTH_SECRET environment variable. Pastikan sudah diatur di file .env");
   }
@@ -16,7 +16,7 @@ export const signJwt = async (
   expiresIn: string | number = "7d"
 ) => {
   const secretKey = new TextEncoder().encode(getJwtSecretKey());
-  
+
   const token = await new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
